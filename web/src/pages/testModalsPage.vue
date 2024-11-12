b8facee309da6bf072d162f57891e4663aed33b2
ira <ira.egoshina@gmail.com>
2024-11-12 17:54:32 +0200

    LP-110 FE: Створити форму для аторізації з полями для вводу інформації

---
+++ web/src/pages/testModalsPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="showModal">Открыть окно авторизации</button>
+    <LoginModal v-if="isModalVisible" :isVisible="isModalVisible"
+      @close="isModalVisible = false"
+      @login="handleLogin" />
+  </div>
+</template>
+
+<script>
+// import LoginModal from './components/LoginModal.vue';
+import LoginModal from '../components/LoginModalComponent.vue';
+
+export default {
+  components: { LoginModal },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    handleLogin(credentials) {
+      console.log('Вход выполнен с данными:', credentials);
+      // Здесь можно добавить логику для обработки входа, например, отправку данных на сервер
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -4,6 +4,7 @@ import {
 } from 'vue-router';
 import MainPage from '@/pages/MainPage.vue';
 import BookPage from '@/pages/BookPage.vue';
+import TestPage from '@/pages/testModalsPage.vue';
 
 export default createRouter({
   history: createWebHistory(),
@@ -17,5 +18,10 @@ export default createRouter({
     path: '/book',
     name: 'BookPage',
     component: BookPage,
+  },
+  {
+    path: '/test',
+    name: 'TestPage',
+    component: TestPage,
   }],
 });
