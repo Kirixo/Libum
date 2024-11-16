5fe83dcb5c2e32d37296c2b04af959dd59be709b
ira <ira.egoshina@gmail.com>
2024-11-16 17:58:47 +0200

    fixed server connection
    fixed routing to main page after sign up/sign in

--- web/vue.config.js
+++ web/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/path': {
-        target: 'https://molly-busy-jolly.ngrok-free.app/api',
+        target: 'https://literate-vastly-pony.ngrok-free.app/api',
         changeOrigin: true,
         pathRewrite: { '^/path': '' }, // Убирает префикс /path из пути запроса
         logLevel: 'debug',  // Добавьте логирование
