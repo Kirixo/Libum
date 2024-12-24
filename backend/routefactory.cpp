8557d3188d05f0ef56ae31254e19b030efa04da9
Kirixo <llkairall1@gmail.com>
2024-12-24 14:00:14 +0200

    Fixed merge issues.

--- backend/routefactory.cpp
+++ backend/routefactory.cpp
@@ -16,20 +16,6 @@ void RouteFactory::registerAllRoutes()
     setupDownloadRoutes();
 }
 
-void RouteFactory::setupUserRoutes() {
-    server_->route("/api/users", QHttpServerRequest::Method::Get, UserHandler::getUser);
-    server_->route("/api/users", QHttpServerRequest::Method::Patch, UserHandler::updateUser);
-    server_->route("/api/users/register", QHttpServerRequest::Method::Post, UserHandler::registerUser);
-    server_->route("/api/users/login", QHttpServerRequest::Method::Post, UserHandler::loginUser);
-    server_->route("/api/users/list", QHttpServerRequest::Method::Get, UserHandler::getUserList);
-}
-
-void RouteFactory::setupBookRoutes() {
-    server_->route("/api/books", QHttpServerRequest::Method::Get, BookHandler::getBook);
-    server_->route("/api/books/list", QHttpServerRequest::Method::Get, BookHandler::getBookList);
-    server_->route("/api/books/genres", QHttpServerRequest::Method::Get, BookHandler::getGenresList);
-}
-
 void RouteFactory::setupBookListRoutes() {
     server_->route("/api/booklist/list", QHttpServerRequest::Method::Get, BookStatusHandler::getStatusList);
     server_->route("/api/booklists/userlist", QHttpServerRequest::Method::Get, BookStatusHandler::getBookListWithStatus);
@@ -62,13 +48,6 @@ void RouteFactory::setupBookRoutes() {
     server_->route("/api/books/list", QHttpServerRequest::Method::Get, BookHandler::getBookList);
     server_->route("/api/books/genres", QHttpServerRequest::Method::Get, BookHandler::getGenresList);
     server_->route("/api/books/file/reader", QHttpServerRequest::Method::Get, BookHandler::getFileForReader);
-}
-
-void RouteFactory::setupCartRoutes() {
-    server_->route("/api/cart/add", QHttpServerRequest::Method::Post, CartHandler::addBook);
-    server_->route("/api/cart", QHttpServerRequest::Method::Get, CartHandler::getUsersCart);
-    server_->route("/api/cart", QHttpServerRequest::Method::Delete, CartHandler::removeBook);
-    server_->route("/api/cart/clear", QHttpServerRequest::Method::Delete, CartHandler::clearCart);
 }
 
 void RouteFactory::handleOptionsRequest()
